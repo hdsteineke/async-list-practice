537f63f4a4342a739e3df51b12fddb92655acd04
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-03-22 14:16:30 -0700

    wrote renderPlant function

--- render-utils.js
+++ render-utils.js
@@ -1,4 +1,4 @@
-export function renderBird(someBird) {
+export function renderBird(somePlant) {
     const birdEl = document.createElement('div');
     const nameEl = document.createElement('h3');
     const imgEl = document.createElement('img');
@@ -21,9 +21,22 @@ export function renderBird(someBird) {
 }
 
 
-// export function renderPlant(somePlant) {
-//     const plantEl = document.createElement('div');
-//     const nameEl = document.createElement('h3');
-//     const imgEl = document.createElement('img');
-//     const nativeEl = document.createElement('p');
-//     const seasonEl = document.createElement('p');
+export function renderPlant(somePlant) {
+    const plantEl = document.createElement('div');
+    const nameEl = document.createElement('h3');
+    const imgEl = document.createElement('img');
+    const nativeEl = document.createElement('p');
+    const seasonEl = document.createElement('p');
+
+    plantEl.classList.add('plant');
+
+    nameEl.textContent = 'Name: ' + somePlant.name;
+    imgEl.src = somePlant.image;
+    nativeEl.textContent = 'PNW Native: ' + somePlant.is_pnw_native;
+    seasonEl.textContent = 'Seasons: ' + somePlant.seasons;
+        
+    plantEl.append(nameEl, imgEl, nativeEl, seasonEl);
+
+    return plantEl;
+
+}
