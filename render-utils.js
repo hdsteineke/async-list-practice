fe21cc274508246b1e63ce387aac5864c14fa440
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-03-22 11:42:36 -0700

    wrote renderBird function in render-utils.js

---
+++ render-utils.js
@@ -0,0 +1,21 @@
+export function renderBird(someBird) {
+    const birdEl = document.createElement('div');
+    const nameEl = document.createElement('h3');
+    const imgEl = document.createElement('img');
+    const colorEl = document.createElement('p');
+    const habitatEl = document.createElement('p');
+    const wingspanEl = document.createElement('p');
+
+    birdEl.classList.add('bird');
+
+    nameEl.textContent = someBird.species;
+    imgEl.src = `./assets/${someBird}-bird.png`;
+    colorEl.textContent = someBird.color;
+    habitatEl.textContent = someBird.habitat;
+    wingspanEl.textContent = someBird.wingspan;
+
+
+    birdEl.append(nameEl, imgEl, colorEl, habitatEl, wingspanEl);
+
+    return birdEl;
+}
